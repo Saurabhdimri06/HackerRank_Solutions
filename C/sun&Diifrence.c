a8cd8c0dbea4508f5f816315cac9ebde2cec6670
Saurabh Dimri <dimrisaurabh06@gmail.com>
2019-07-13 10:37:40 +0530

    Basic Sum and Diffrence Code

---
+++ C/sun&Diifrence.c
@@ -0,0 +1,22 @@
+//Sum and Diffrence
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a,b;
+    float c,d;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%f",&c);
+    scanf("%f",&d);
+    printf("%d ",a+b);
+    printf("%d \n",a-b);
+    printf("%.1f ",c+d);
+    printf("%.1f",c-d);
+    return 0;
+}
+
