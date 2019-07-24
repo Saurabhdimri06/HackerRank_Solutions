752ed1df20a5bb0053d55d04e3970faa1925ebbf
Saurabh Dimri <dimrisaurabh06@gmail.com>
2019-07-24 22:20:47 +0530

    A basic square generator

---
+++ Python/Squaring_Numbers.py
@@ -0,0 +1,11 @@
+#Task
+
+#Read an integer N. For all non-negative integers i<N, print i^2. See the sample for details.
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#SOLUTION
+if __name__ == '__main__':
+    n = int(raw_input())
+    for i in range(n):
+        print"{}".format(i**2)
